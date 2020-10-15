62413dd02f3e493a7c7c9779dc15fa87f324a797
yangjun <yangjun1@meizu.com>
2020-10-15 14:34:50 +0800

    112

--- pages/hooks.jsx
+++ pages/hooks.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import request from 'umi-request';
 import { server } from '../config';
-// import fetch from 'fetch'
 import Layout, { siteTitle } from '../components/layout'
 
 const Hooks = ({stars}) =>{
