9cb842abc35d5d6f6dcce15ce3e451dd3a9735b5
yangjun <yangjun1@meizu.com>
2020-10-16 18:08:11 +0800

    数据库

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,4 @@
 export default (req, res) => {
   res.status(200).json({ text: 'Hello' })
 }
+vercel secrets add my-mongodb-uri mongodb+srv://<user>:<password>@my-cluster-uf345.mongodb.net/<database-name>?retryWrites=true
--- pages/api/movi.js
+++ pages/api/movi.js
@@ -1,3 +1,8 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+
 const list = {
     subjects: [
     {
@@ -554,6 +559,19 @@ const list = {
 }
 
 export default (req, res) => {
+    client.connect(err => {
+        // perform actions on the collection object
+        const collection = client.db("sample_mflix");
+
+        console.log('链接数据222库成功',collection)
+
+        collection.collection("movies").find({}).toArray(function(err, result) { // 返回集合中所有数据
+            console.log(result,err);
+            client.close();
+        });
+        // client.close();
+        console.log(err)
+      });
     res.status(200).json(list)
   }
   
